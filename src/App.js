c4c755b669471b5723a4b23f611fea9132e3f00a
Thomas <malifarge.thomas9@gmail.com>
2022-07-18 14:39:40 +0200

    form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Form from "./Form";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const regex = /^[\w\.]+@([\w-]+\.)+[\w-]{2,4}$/
+
+class App extends React.Component{
+
+  constructor(){
+    super()
+
+    this.state = {
+      email: "",
+      password: "",
+      rememberMe: false,
+      emailIsValid: false,
+      passwordIsValid: false,
+      isSubmitted: false,
+      firstName: "",
+      lastName: ""
+    }
+  }
+
+  handleEmailChange = e =>{
+    const mailTest = regex.test(e.target.value)
+    if (mailTest){
+      this.setState({
+        email: e.target.value,
+        emailIsValid: true
+      })
+    }else {
+      this.setState({
+        email: "",
+        emailIsValid: false
+      })
+    }
+  }
+
+  handlePasswordChange = e =>{
+    if (e.target.value.length>5){
+      this.setState({
+        password: e.target.value,
+        passwordIsValid: true
+      })
+    }else {
+      this.setState({
+        password: "",
+        passwordIsValid: false
+      })
+    }
+  }
+
+  handleRememberMeChange = e =>{
+    this.setState({
+      rememberMe : e.target.checked
+    })
+  }
+
+  handleSubmit = e =>{
+    e.preventDefault();
+    if (this.state.emailIsValid && this.state.passwordIsValid){
+      this.setState({
+        isSubmitted : true
+      })
+    }else {
+      this.setState({
+        isSubmitted : false
+      })
+    }
+    console.log(this.state);
+  }
+
+  handleFirstNameChange = e =>{
+    this.setState({
+      firstName : e.target.value
+    })
+  }
+
+  handleLastNameChange = e =>{
+    this.setState({
+      lastName : e.target.value
+    })
+  }
+
+
+  render() {
+    return (
+      
+          <section>
+            <h1>Login</h1>
+            {this.state.isSubmitted ? <p>Welcom {this.state.firstName} {this.state.lastName}</p> : <Form mailChange={this.handleEmailChange} passwordChange={this.handlePasswordChange} checkChange={this.handleRememberMeChange} submit={this.handleSubmit} firstName={this.handleFirstNameChange} lastName={this.handleLastNameChange}/>}
+          </section>
+
+    )
+  }
 }
 
 export default App;
---
+++ src/Form.js
@@ -0,0 +1,65 @@
+import React from "react";
+
+class Form extends React.Component{
+    render() {
+        return (
+            <form onSubmit={this.props.submit}>
+            
+            {/* Firstname Name */}
+
+            <div className="form-row">
+
+              {/* Firstname */}
+
+
+                  <div className="col-md-6 mb-3">
+                    <label for="validationCustom01">First name</label>
+                    <input type="text" className="form-control" id="validationCustom01" placeholder="First name..." required onChange={this.props.firstName}/>
+                    <div className="valid-feedback">
+                      Looks good!
+                  </div>
+
+              {/* Name */}
+
+                  </div>
+                  <div className="col-md-6 mb-3">
+                    <label for="validationCustom02">Last name</label>
+                    <input type="text" className="form-control" id="validationCustom02" placeholder="Last name..." required onChange={this.props.lastName}/>
+                    <div className="valid-feedback">
+                      Looks good!
+                    </div>
+                  </div>
+              </div>
+
+            {/* Mail */}
+
+            <div className="form-group">
+              <label for="exampleInputEmail1">Email address</label>
+              <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email..." onChange={this.props.mailChange}/>
+              <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+            </div>
+
+            {/* Password */}
+
+            <div className="form-group">
+              <label for="exampleInputPassword1">Password</label>
+              <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Enter Password..." onChange={this.props.passwordChange}/>
+            </div>
+
+            {/* Remember me */}
+
+
+            <div className="form-check">
+              <input type="checkbox" className="form-check-input" id="exampleCheck1" onChange={this.props.checkChange}/>
+              <label className="form-check-label" for="exampleCheck1">Remember me</label>
+            </div>
+
+            {/* Submit */}
+
+            <button type="submit" className="btn btn-primary">Submit</button>
+          </form>
+        );
+    }
+}
+
+export default Form
